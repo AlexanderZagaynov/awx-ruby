a1931d8ea99fe0d7dfba736dcd2b444290a5aede
Alexander Zagaynov <zalex80@gmail.com>
2019-10-24 13:43:26 +0200

    bundle gem awx-api --no-exe --no-coc --no-ext --mit --test=rspec

---
+++ lib/awx/api.rb
@@ -0,0 +1,8 @@
+require "awx/api/version"
+
+module Awx
+  module Api
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/awx/api/version.rb
@@ -0,0 +1,5 @@
+module Awx
+  module Api
+    VERSION = "0.1.0"
+  end
+end
