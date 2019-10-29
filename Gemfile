25a562441c2bd0a340905da66b0cddc5c40a75ac
Alexander Zagaynov <zalex80@gmail.com>
2019-10-29 14:21:10 +0100

    run rubocop with github actions

--- Gemfile
+++ Gemfile
@@ -2,3 +2,9 @@
 
 source 'https://rubygems.org'
 gemspec
+
+# group :rubocop, optional: true do
+#   gem 'rubocop'
+#   gem 'rubocop-rspec'
+#   gem 'rubocop-performance'
+# end
--- spec/awx/api_spec.rb
+++ spec/awx/api_spec.rb
@@ -4,4 +4,7 @@ RSpec.describe Awx::Api do
   it 'has a version number' do
     expect(Awx::Api::VERSION).to_not be nil
   end
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
 end
